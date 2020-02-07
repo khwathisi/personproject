3c0023657c8e7eaf87d17efc30685259b4f3febf
Khwathisi <godfrey.khwathisi@umuzi.org>
2020-02-07 16:44:20 +0200

    Updated Bro

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -2,7 +2,7 @@ public class MainProgram {
     public static void main(String args[]) {
 
 
-        Person person=new Person("Ryan", 30, "haile",new String[]{
+        Person person=new Person("Ryan", 30, "female", new String[]{
                 "Hardarse", "agile", "ssd hard drives"
         });
 
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
+
 public class Person {
 
     private String name;
     private int age;
     private String gender;
-    private String[] interests;
+    String[] interests;
 
     public Person(String name, int age, String gender, String[] interests) {
         this.name = name;
@@ -20,8 +23,22 @@ public class Person {
 
 
     public String Hello() {
-        String txt="Hello, My name is: "+ name+" I am " + age +" years old." +
-                " I am " + gender +", My interests are "+ interests[0]+", "+interests[1]+", "+interests[2];
+        int len = interests.length - 1;
+        String lastText = interests[len].toString();
+
+
+        ArrayList last = new ArrayList();
+
+        for(int i=0; i< interests.length - 1; i++){
+            last.add(interests[i]);
+        }
+
+        String text = last.toString();
+        text = text.substring(1, text.length() - 1);
+
+
+        String txt="Hello, My name is "+ name+" and I am " + age +" years old." +
+                " My interests are being a "+ text + " and " + lastText + ".";
        return txt;
     }
 
